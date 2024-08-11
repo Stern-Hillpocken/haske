e41a6fef842c9f2e1c37ce5424265a8146360dc9
Nicolas Guérin <nicolas.guerin.pro@laposte.net>
2024-08-11 14:36:39 +0200

    First time tracker

--- src/app/models/game-state.model.ts
+++ src/app/models/game-state.model.ts
@@ -1,8 +1,13 @@
+import { GameTime } from "./game-time.model";
+
 export class GameState {
     constructor(
         public menuChoice: string,
         public windowStart: string,
         public objectDragged: string,
-        public windowEnd: string
+        public windowEnd: string,
+
+        public flame: number,
+        public time: GameTime
     ) {}
 }
