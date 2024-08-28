63c64dba315a0499add6c9bcac77df8b3a899ed0
Nicolas Guérin <nicolas.guerin.pro@laposte.net>
2024-08-28 22:17:58 +0200

    Time-tracker display little by little and notes are send as the same speed

--- src/app/components/ui/game/time-banner/time-banner.component.ts
+++ src/app/components/ui/game/time-banner/time-banner.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { GameTime } from 'src/app/models/game-time.model';
 
 @Component({
   selector: 'app-time-banner',
@@ -8,28 +9,25 @@ import { Component, Input } from '@angular/core';
 export class TimeBannerComponent {
 
   @Input()
-  tick!: number;
-
-  @Input()
-  speed!: number;
+  time!: GameTime
 
   title: string = "";
   content: string = "";
 
   isTimeEvent(): boolean {
-    if ([5, 6, 7, 8, 9].includes(this.tick)) {
+    if (this.time.day >= 3 && [5, 6, 7, 8, 9].includes(this.time.tick)) {
       this.title = "Évènement environemental";
       this.content = "Un nouvel évènement vient d’arriver";
       return true;
-    } else if ([75, 76, 77, 78, 79].includes(this.tick)) {
+    } else if (this.time.day >= 2 && [75, 76, 77, 78, 79].includes(this.time.tick)) {
       this.title = "Nouveaux et nouvelles arrivantes";
       this.content = "Des personnes sont arrivées dans le phare";
       return true;
-    } else if ([85, 86, 87, 88, 89].includes(this.tick)) {
+    } else if (this.time.day >= 4 && [85, 86, 87, 88, 89].includes(this.time.tick)) {
       this.title = "Attaque des créatures";
       this.content = "Si tu ne les gères pas elles éteindront le phare";
       return true;
-    } else if ([100, 0, 1, 2, 3].includes(this.tick)) {
+    } else if ([100, 0, 1, 2, 3].includes(this.time.tick)) {
       this.title = "La flamme diminue";
       this.content = "Le phare perd 1 de lueur";
       return true;
