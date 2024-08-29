187742fe717158a4d73eac169fc6125141522313
Nicolas Guérin <nicolas.guerin.pro@laposte.net>
2024-08-29 20:41:57 +0200

    Mine can be find during exploration. Rate are applied for what worker can find. It could be nothing.

--- src/app/models/game-window.model.ts
+++ src/app/models/game-window.model.ts
@@ -24,7 +24,7 @@ export class GameWindowStorage extends GameWindow {
     constructor(
         public override name: WindowNames = "storage",
         public override content: DraggableNames[] = [],
-        public override acceptance: DraggableNames[] = ["stone", "water", "wood", "note-help-and-trash", "note-event-event", "note-event-newcomers", "note-event-fight", "note-event-end-day", "note-storage-filter", "note-time-strip"],
+        public override acceptance: DraggableNames[] = ["stone", "water", "wood", "iron-ore", "note-help-and-trash", "note-event-event", "note-event-newcomers", "note-event-fight", "note-event-end-day", "note-storage-filter", "note-time-strip"],
         public override maxSpace: number = 14,
         public override slot: ResourceNames[] = []
     ) {
@@ -58,7 +58,7 @@ export class GameWindowQuarry extends GameWindow {
     constructor(
         public override name: WindowNames = "quarry",
         public override content: DraggableNames[] = [],
-        public override acceptance: DraggableNames[] = ["worker"],
+        public override acceptance: DraggableNames[] = ["worker", "miner"],
         public override currentTime: number = 0,
         public override maxTime: number = 8,
         public override usageRemaining: number =  random(2,5)
@@ -71,10 +71,23 @@ export class GameWindowScrub extends GameWindow {
     constructor(
         public override name: WindowNames = "scrub",
         public override content: DraggableNames[] = [],
-        public override acceptance: DraggableNames[] = ["worker"],
+        public override acceptance: DraggableNames[] = ["worker", "miner"],
         public override currentTime: number = 0,
         public override maxTime: number = 8,
         public override usageRemaining: number =  random(2,5)
+    ) {
+        super(name, content, acceptance)
+    }
+}
+
+export class GameWindowMine extends GameWindow {
+    constructor(
+        public override name: WindowNames = "mine",
+        public override content: DraggableNames[] = [],
+        public override acceptance: DraggableNames[] = ["miner"],
+        public override currentTime: number = 0,
+        public override maxTime: number = 12,
+        public override usageRemaining: number =  random(2,4)
     ) {
         super(name, content, acceptance)
     }
@@ -95,7 +108,7 @@ export class GameWindowHelp extends GameWindow {
     constructor(
         public override name: WindowNames = "help",
         public override content: DraggableNames[] = [],
-        public override acceptance: DraggableNames[] = ["worker", "stone", "water", "wood", "note-help-and-trash", "note-event-event", "note-event-newcomers", "note-event-fight", "note-event-end-day", "note-storage-filter", "note-time-strip"],
+        public override acceptance: DraggableNames[] = ["worker", "stone", "water", "wood", "iron-ore", "note-help-and-trash", "note-event-event", "note-event-newcomers", "note-event-fight", "note-event-end-day", "note-storage-filter", "note-time-strip"],
         public override slot: DraggableNames[] = ["note-help-and-trash"]
     ) {
         super(name, content, acceptance)
--- src/app/types/window-names.type.ts
+++ src/app/types/window-names.type.ts
@@ -1,1 +1,1 @@
-export type WindowNames = "lighthouse" | "exploration" | "storage" | "quarry" | "scrub" | "workbench" | "trash" | "help" | "recipes-book" | "dressing";
+export type WindowNames = "lighthouse" | "exploration" | "storage" | "quarry" | "scrub" | "workbench" | "trash" | "help" | "recipes-book" | "dressing" | "mine";
